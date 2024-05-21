a9168ee647acdd669e20e0ce17ebca3d4aec507f
Ayatulloh Ahad R <kawoel@gmail.com>
2024-05-22 03:28:22 +0700

    update sql dumper

--- mysql/new_dump.sh
+++ mysql/new_dump.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Generate nama file dengan format yyyymmdd
-filename="backup_$(date +%Y%m%d).sql"
+filename="db_dump_$(date +%Y%m%d).sql"
 
-# Eksekusi perintah mysqldump
-mysqldump -u root sql_cloudtrader23 --default-character-set=utf8 -r "$filename"
+# Eksekusi perintah mariadb-dump --skip-lock-tables --routines --add-drop-table --disable-keys --extended-insert --events --comments -u root --host=localhost --port=3306 binary_v2
+mariadb-dump -u root -p binary_v2 --default-character-set=utf8 --skip-lock-tables --routines --add-drop-table --disable-keys --extended-insert --events --comments -r "$filename"
+
+# remove definer
+sed -i 's/DEFINER=[^*]*\*/\*/g' "$filename"
